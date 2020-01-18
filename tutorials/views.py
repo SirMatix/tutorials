16891be41f2b375ae8dd4a5d47e76378907dd29a
SirMatix <54940579+SirMatix@users.noreply.github.com>
2020-01-18 11:05:26 +0000

    add request form and view

--- tutorials/views.py
+++ tutorials/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Tutorial
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import logout, authenticate, login
+
 
 def homepage(request):
-	return render(request=request, 
-				  template_name="tutorials/home.html",
-				  context={"tutorials": Tutorial.objects.all})
+    return render(request=request,
+                  template_name="tutorials/home.html",
+                  context={"tutorials": Tutorial.objects.all})
+
+def register(request):
+    form = UserCreationForm
+    return render(request, 
+                  "tutorials/register.html",
+                  {"form":form})
+
