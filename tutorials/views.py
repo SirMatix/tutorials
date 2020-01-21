0590650bcbb013fa627a5f75dfcad8ec27a7dd2b
SirMatix <54940579+SirMatix@users.noreply.github.com>
2020-01-21 13:30:07 +0000

    Add tutorial.html template, Add sidebar to navigating throught tutorials

--- tutorials/views.py
+++ tutorials/views.py
@@ -20,19 +20,34 @@ def single_slug(request, single_slug):
         series_urls = {}
         # we are sending user to part one of tutorial of matching series
         for m in matching_series.all():
-            # filtering foreign key in tutorials matching to TutorialSeries series variable
-            # part_one returns the earliest tutorial of particular series
+            # filtering tutorials and matching a tutorial foreign key to TutorialSeries series variable
+            # part_one returns the earliest tutorial(object) of particular series
             part_one = Tutorial.objects.filter(series__series=m.series).earliest("published")
-            # key is the object(series), value is url(slug)
+            # key is the object(series), value is tutorial url(slug)
             series_urls[m] = part_one.slug
         # in the end we return a render
         return render(request=request,
                       template_name="tutorials/category.html",
                       context={"part_ones": series_urls})
 
-    turorials = [t.slug for t in Tutorial.objects.all()]
-    if single_slug in turorials:
-        return HttpResponse(f"{single_slug} is a tutorial!!!")
+    tutorials = [t.slug for t in Tutorial.objects.all()]
+    if single_slug in tutorials:
+        # finding the tutorial that matches single_slug
+        this_tutorial = Tutorial.objects.get(slug=single_slug)
+        # filtering tutorial(object) matching its foreign key(series) to TutorialSeries(object) series attribute using double underscore
+        # find Tutorial series where TutorialSeries series is equal to this_tutorial.series
+        # the put that in the order by the published date
+        tutorial_from_series = Tutorial.objects.filter(series__series=this_tutorial.series).order_by("published")
+
+        # getting index of this tutorial value by switch tutorial_series into list
+        this_tutorial_idx = list(tutorial_from_series).index(this_tutorial)
+
+        return render(request = request,
+                      template_name='tutorials/tutorial.html',
+                      context = {"tutorial":this_tutorial,
+                                "sidebar": tutorial_from_series,
+                                "this_tutorial_idx": this_tutorial_idx})
+
 
     return HttpResponse(f"{single_slug} doesn't correspond to anything")
 
@@ -59,11 +74,9 @@ def register(request):
         else:     # if form is not valid
             for msg in form.error_messages:
                 messages.error(request, f"{msg}: {form.error_messages[msg]}")
-
             return render(request =request, 
                          template_name="tutorials/register.html",
                          context={"form":form})
-
     form = NewUserForm
     return render(request = request,
                   template_name = "tutorials/register.html",
@@ -96,9 +109,9 @@ def login_request(request):
         else:
             messages.error(request, "Invalid username or password")
     form = AuthenticationForm()
-    return render(request, 
-                  "tutorials/login.html",
-                  {"form":form})
+    return render(request=request, 
+                  template_name="tutorials/login.html",
+                  context={"form":form})
 
 
 
